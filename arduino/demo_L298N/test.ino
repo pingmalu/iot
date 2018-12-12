a720b00a95d8799b6904f93941eacf2c1d9d02d7
malu <malu@malu.me>
2018-12-12 22:22:06 +0800

    Create test.ino

---
+++ arduino/demo_L298N/test.ino
@@ -0,0 +1,50 @@
+/**
+ * L298N模块，调速测试
+ * 参考教程 https://blog.csdn.net/ace0perfect/article/details/79221628
+ */
+
+int IN1 = 4;
+int IN2 = 5;
+//int ENA = 6;
+
+unsigned long time = 1200;  //delay time
+int value = 150;   // the duty cycle 100-150
+
+
+void setup() { 
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+ 
+    //set direction
+//    digitalWrite(IN1, HIGH);
+//    digitalWrite(IN2, LOW);
+}
+
+void loop() { 
+//   analogWrite(ENA, value);
+//delay(time);
+
+  
+//  int value;
+
+  digitalWrite(IN1, HIGH);   
+//  digitalWrite(IN2, LOW);   
+  analogWrite(IN2, 200);   
+////  digitalWrite(IN2, HIGH);
+//  analogWrite(IN2, 50);
+    delay(3000);
+//
+//  digitalWrite(IN1, LOW);   
+//  digitalWrite(IN2, LOW);   
+//    delay(3000);
+//
+    digitalWrite(IN1, LOW);   
+  analogWrite(IN2, 50);
+    delay(3000);
+
+//  for(value=0;value<=100;value+=5){
+//    digitalWrite(IN1, LOW);   
+//    analogWrite(IN2, value);       
+//    delay(130);
+//  }
+}
