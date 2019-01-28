14c852a03cb673a86cfef0a1e789489a758f51ab
malu <malu@malu.me>
2019-01-28 18:00:17 +0800

    redrcv

--- arduino/demo_nrf2401/demo_nrf2401.ino
+++ arduino/demo_nrf2401/demo_nrf2401.ino
@@ -20,7 +20,7 @@
 
 const int VRX=A0;
 const int VRY=A1;
-const int SW = 5;
+const int SW = 6;
 const int X0=525;
 const int Y0=496;
 byte joyStick[2];
@@ -28,10 +28,10 @@ byte joyStick[2];
 void setup(){
   Serial.begin(9600);
   
-  pinMode(SW, INPUT);
+  pinMode(2, INPUT);
   pinMode(VRX, INPUT);
   pinMode(VRY, INPUT);
-  digitalWrite(SW, LOW);
+  // digitalWrite(SW, LOW);
 
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
@@ -46,28 +46,33 @@ void loop() {
 
   int val_x =map(analogRead(VRX),0,1024,0,255);
   int val_y=map(analogRead(VRY),0,1024,0,255);
+  int val_sw = digitalRead(2);
 
   //技巧:有多少个数就用多少个数组单元去储存
   
   joyStick[0] = val_x;
-  joyStick[1] = val_y; 
+  joyStick[1] = val_y;
 
   //这个时间值用于判断是否连接超时
   //unsigned long time = millis();
   //long time_long=long(time);
   
-  Serial.print("X:");
+  Serial.print("SW:");
+  Serial.print(val_sw);
+  Serial.print(" X:");
   Serial.print(val_x);
   Serial.print(" Y:");
   Serial.println(val_y);
 
-  //设置当前发射器的地址 
-  Mirf.setTADDR((byte *)"serv1");
+  delay(50);
 
-  //将读入的值写到SPI中
-  Mirf.send((byte *) &joyStick);
+  // //设置当前发射器的地址 
+  // Mirf.setTADDR((byte *)"serv1");
 
-  while(Mirf.isSending()) {
-      // 如果正在发送时什么都不做，这里只是一个等待，等isSending完成后才能退出循环
-  }
+  // //将读入的值写到SPI中
+  // Mirf.send((byte *) &joyStick);
+
+  // while(Mirf.isSending()) {
+  //     // 如果正在发送时什么都不做，这里只是一个等待，等isSending完成后才能退出循环
+  // }
 }
