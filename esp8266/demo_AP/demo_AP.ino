074c3725d8ee658382322af50d3f375ac36fb46c
malu <malu@malu.me>
2019-11-28 16:38:49 +0800

    ap

--- esp8266/demo_AP/demo_AP.ino
+++ esp8266/demo_AP/demo_AP.ino
@@ -1,9 +1,17 @@
 #include <ESP8266WiFi.h>
 
+extern "C"
+{
+#include <user_interface.h>
+}
+
 void setup()
 {
     Serial.begin(115200);
     Serial.println();
+
+    uint8_t macAddr[6]{0x88, 0xd7, 0x63, 0x00, 0x0e, 0x02};
+    wifi_set_macaddr(SOFTAP_IF, macAddr);
 
     Serial.print("Setting soft-AP ... ");
     Serial.println(WiFi.softAP("M4", "ifconfig") ? "Ready" : "Failed!");
