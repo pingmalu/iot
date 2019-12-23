b3f6cc8c576ca2145895497a4ef498360ad39c6f
malu <malu@malu.me>
2019-12-24 00:27:53 +0800

    gsc1

--- 项目/Tinker_Motor_Car_esp8266/Tinker_Motor_Car_esp8266.ino
+++ 项目/Tinker_Motor_Car_esp8266/Tinker_Motor_Car_esp8266.ino
@@ -5,11 +5,16 @@
 #include <Servo.h> // 舵机控制
 Servo SV1;         // 创建舵机对象
 Servo SV2;         // 创建舵机对象
+Servo SV3;         // 创建舵机对象
+Servo SV4;         // 创建舵机对象
 ////////////////////////////--------------------Variables-------------------------------------------/////////////////////////////////////
 WiFiUDP Udp;
 const int LED = LED_BUILTIN;
 const int Y_PIN = D1;
 const int X_PIN = D2;
+const int Y_PIN_R = D3;
+const int X_PIN_R = D4;
+const int LED_2 = D8;
 ////////////////////////////--------------------Don't change these Settings-------------------------------------------/////////////////////////////////////
 /* Don't change this stuff(because the phone will be looking for us based on SSID name and port) */ //Note, we are not connecting to WiFi, because we are looking to optimize speed.  You can connect to WiFi if you want
 const char *ssid = "ESPTest";
@@ -35,12 +40,29 @@ void setup()
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
+  pinMode(LED_2, OUTPUT);
+  digitalWrite(LED_2, LOW);
 
   // 舵机引脚初始化
   SV1.attach(Y_PIN);
   SV2.attach(X_PIN);
+  SV3.attach(Y_PIN_R);
+  SV4.attach(X_PIN_R);
 }
 
+bool led2 = false;
+void led2f()
+{
+  if (led2)
+  {
+    digitalWrite(LED_2, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_2, LOW);
+  }
+  led2 = led2 ? false : true;
+}
 ////////////////////////////--------------------LOOP-------------------------------------------/////////////////////////////////////
 
 void loop()
@@ -83,25 +105,65 @@ void useGameData(char type, int value)
     {
     case 1:
       digitalWrite(LED, LOW);
+      Serial.print("led2 low");
       break;
     case 0:
       digitalWrite(LED, HIGH);
+      Serial.print("led2 high");
       break;
     }
     break;
 
-  //left joystick Y direction
-  case 'l':
+  //left joystick X direction
+  case 'k':
     //this will bring it down to a range 0-255;
-    num = map(value, 0, 255, 0, 180);
+    num = map(value, 100, 355, 180, 0);
     SV1.write(num);
     break;
 
+  //left joystick X direction
+  case 'l':
+    //this will bring it down to a range 0-255;
+    num = map(value, 100, 355, 180, 0);
+    SV2.write(num);
+    break;
   //Right joystick X direction
   case 'z':
-    //this will bring it down to a range 0-255;
-    num = map(value, 0, 255, 0, 180);
-    SV2.write(num);
+    num = map(value, 100, 355, 120, 60);
+    SV3.write(num);
+    break;
+  //Right joystick Y direction
+  case 'x':
+    num = map(value, 100, 355, 120, 60);
+    SV4.write(num);
+    break;
+
+    //-------------Dpad Buttons------------//
+  case 'd':
+    switch (value)
+    {
+      //Dpad Center Button
+    case 0:
+      led2f();
+      // Serial.println("D-pad Center");
+      break;
+    //Dpad Left Button
+    case 1:
+      // Serial.println("D-pad Left");
+      break;
+    //Dpad Up Button
+    case 2:
+      // Serial.println("D-pad Up");
+      break;
+    //Dpad Right Button
+    case 3:
+      // Serial.println("D-pad Right");
+      break;
+    //Dpad Down Button
+    case 4:
+      // Serial.println("D-pad Down");
+      break;
+    }
     break;
   }
   yield();
