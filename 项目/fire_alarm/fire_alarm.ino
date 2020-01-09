d6c21dcb5921e8517b63f0fcb67302225a242082
malu <malu@malu.me>
2020-01-09 15:37:50 +0800

    poweroff

--- 项目/fire_alarm/fire_alarm.ino
+++ 项目/fire_alarm/fire_alarm.ino
@@ -13,7 +13,7 @@
 
 int buzzer_PIN = 5;       //设置蜂鸣器的数字引脚为8
 int flame_sensor_PIN = 4; //设置火焰传感器的数字引脚为4
-int flame_detected;   //定义一个变量
+int flame_detected;       //定义一个变量
 
 // 休眠
 void go_poweroff()
@@ -26,7 +26,7 @@ void setup() //初始化
 {
     Serial.begin(9600);
     pinMode(buzzer_PIN, OUTPUT);      //将8号引脚设置为输出信号
-    pinMode(LED_BUILTIN, OUTPUT);         //将13号引脚设置为输出信号
+    pinMode(LED_BUILTIN, OUTPUT);     //将13号引脚设置为输出信号
     pinMode(flame_sensor_PIN, INPUT); //将4号引脚设置为输入信号
     digitalWrite(buzzer_PIN, LOW);
 }
