d41697f319b765a2ddd4718a565773c1fd23b876
malu <malu@malu.me>
2020-02-17 16:42:04 +0800

    a

---
+++ esp8266/smart_config/smart_config.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+    Serial.begin(115200);
+    /* Set ESP8266 to WiFi Station mode */
+    WiFi.mode(WIFI_STA);
+    /* start SmartConfig */
+    WiFi.beginSmartConfig();
+
+    /* Wait for SmartConfig packet from mobile */
+    Serial.println("Waiting for SmartConfig.");
+    while (!WiFi.smartConfigDone())
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("SmartConfig done.");
+
+    Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
+    Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
+
+    /* Wait for WiFi to connect to AP */
+    Serial.println("Waiting for WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi Connected.");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+void loop()
+{
+}
