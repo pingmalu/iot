a37649a0d921f1b69217fa77957c8e803c67bc51
malu <malu@malu.me>
2020-05-01 18:06:20 +0800

    autopush

--- 项目/Little_star_esp8266/Little_star_esp8266.ino
+++ 项目/Little_star_esp8266/Little_star_esp8266.ino
@@ -14,28 +14,87 @@ void setup()
 
 void loop()
 {
-    for (int i = 0; i < 1023; i++)
+    for (int i = 0; i < 823; i++)
     {
-        analogWrite(D0, i);delay(random(1,4));
-        analogWrite(D1, i);delay(random(1,4));
-        analogWrite(D2, i);delay(random(1,4));
-        analogWrite(D3, i);delay(random(1,4));
-        analogWrite(D4, i);delay(random(1,4));
-        analogWrite(D5, i);delay(random(1,4));
-        analogWrite(D6, i);delay(random(1,4));
-        analogWrite(D7, i);delay(random(1,4));
-        analogWrite(D8, i);delay(random(1,4));
+        analogWrite(D0, i);
+        analogWrite(D1, i);
+        analogWrite(D2, i);
+        analogWrite(D3, i);
+        analogWrite(D4, i);
+        analogWrite(D5, i);
+        analogWrite(D6, i);
+        analogWrite(D7, i);
+        analogWrite(D8, i);
+        delay(random(1, 4));
     }
-    for (int i = 1023; i > 0; i--)
+    for (int i = 823; i > 0; i--)
     {
-        analogWrite(D0, i);delay(random(1,4));
-        analogWrite(D1, i);delay(random(1,4));
-        analogWrite(D2, i);delay(random(1,4));
-        analogWrite(D3, i);delay(random(1,4));
-        analogWrite(D4, i);delay(random(1,4));
-        analogWrite(D5, i);delay(random(1,4));
-        analogWrite(D6, i);delay(random(1,4));
-        analogWrite(D7, i);delay(random(1,4));
-        analogWrite(D8, i);delay(random(1,4));
+        analogWrite(D0, i);
+        analogWrite(D1, i);
+        analogWrite(D2, i);
+        analogWrite(D3, i);
+        analogWrite(D4, i);
+        analogWrite(D5, i);
+        analogWrite(D6, i);
+        analogWrite(D7, i);
+        analogWrite(D8, i);
+        delay(random(1, 4));
     }
+    int i = 1023;
+    int t;
+    t = random(10,200);
+    analogWrite(D0, i);delay(t);
+    analogWrite(D1, i);delay(t);
+    analogWrite(D2, i);delay(t);
+    analogWrite(D3, i);delay(t);
+    analogWrite(D4, i);delay(t);
+    analogWrite(D5, i);delay(t);
+    analogWrite(D6, i);delay(t);
+    analogWrite(D7, i);delay(t);
+    analogWrite(D8, i);delay(t);
+    i = 0;
+    analogWrite(D8, i);delay(t);
+    analogWrite(D7, i);delay(t);
+    analogWrite(D6, i);delay(t);
+    analogWrite(D5, i);delay(t);
+    analogWrite(D4, i);delay(t);
+    analogWrite(D3, i);delay(t);
+    analogWrite(D2, i);delay(t);
+    analogWrite(D1, i);delay(t);
+    analogWrite(D0, i);delay(t);
+
+
+
+    i = 1023;
+    t = random(10,200);
+    analogWrite(D8, i);delay(t);
+    analogWrite(D7, i);delay(t);
+    analogWrite(D6, i);delay(t);
+    analogWrite(D5, i);delay(t);
+    analogWrite(D4, i);delay(t);
+    analogWrite(D3, i);delay(t);
+    analogWrite(D2, i);delay(t);
+    analogWrite(D1, i);delay(t);
+    analogWrite(D0, i);delay(t);
+    i = 0;
+    analogWrite(D0, i);delay(t);
+    analogWrite(D1, i);delay(t);
+    analogWrite(D2, i);delay(t);
+    analogWrite(D3, i);delay(t);
+    analogWrite(D4, i);delay(t);
+    analogWrite(D5, i);delay(t);
+    analogWrite(D6, i);delay(t);
+    analogWrite(D7, i);delay(t);
+    analogWrite(D8, i);delay(t);
+
+
+    // digitalWrite(D0,random(0,1));delay(200);
+    // digitalWrite(D1,random(0,1));delay(200);
+    // digitalWrite(D2,random(0,1));delay(200);
+    // digitalWrite(D3,random(0,1));delay(200);
+    // digitalWrite(D4,random(0,1));delay(200);
+    // digitalWrite(D5,random(0,1));delay(200);
+    // digitalWrite(D6,random(0,1));delay(200);
+    // digitalWrite(D7,random(0,1));delay(200);
+    // digitalWrite(D8,random(0,1));delay(200);
 }
